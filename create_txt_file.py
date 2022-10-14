5db1e040824ff9a73143c2a2866c3f864bbec7ab
derekYankie <derek.afriyie@gmail.com>
2022-10-14 17:07:46 -0400

    update python script

--- create_txt_file.py
+++ create_txt_file.py
@@ -1,2 +1,2 @@
 with open('job1.txt', 'w') as f:
-    f.write("Writing data to file...\nThis sentence is coming from a file called job1.txt\nDone! ^_^")
+    f.write("This sentence is coming from a file called job1.txt\nDone! ^_^")
